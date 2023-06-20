00fcc7a4b5b743913cbd2baf04b6ce19f25bd26c
sukhvinder-jaat <monudhaka829@gmail.com>
2023-06-20 10:10:17 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { SliderSecTwo } from "./components/SliderSecTwo";
 function App() {
   return (
--- src/components/SliderSecTwo.jsx
+++ src/components/SliderSecTwo.jsx
@@ -1,10 +1,63 @@
 import React from "react";
-
+// import Slider from "react-slick";
 export const SliderSecTwo = () => {
+  // var our_slider = {
+  //   dots: false,
+  //   infinite: true,
+  //   arrows: false,
+  //   speed: 500,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 2.5,
+  //         slidesToScroll: 1,
+  //         centerMode: true,
+  //         centerPadding: 60,
+  //         infinite: true,
+  //         dots: false,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 1.5,
+  //         centerMode: true,
+  //         centerPadding: 60,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 576,
+  //       settings: {
+  //         dots: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 450,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         centerMode: false,
+  //         dots: true,
+  //         //   centerPadding: 60,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // };
+  // const buttons = React.useRef();
   return (
-    <div className="d-flex">
-      <p>SliderSecTwo</p>
-      <p>SliderSecTwo</p>
+    <div className="">
+      <div className="slider_container"></div>
     </div>
   );
 };
