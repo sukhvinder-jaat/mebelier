7de208d3f4ef51b98c5c85f9abd5664bd395c9ba
sukhvinder-jaat <monudhaka829@gmail.com>
2023-06-20 09:52:39 +0530

    root

---
+++ src/components/SliderSecTwo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SliderSecTwo = () => {
+  return (
+    <div className="d-flex">
+      <p>SliderSecTwo</p>
+      <p>SliderSecTwo</p>
+    </div>
+  );
+};
