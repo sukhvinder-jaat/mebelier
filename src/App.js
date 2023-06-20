c28164c848bb1c2193bc55b6dbe6079e03971751
ashok-kumar-032 <av6017819@gmail.com>
2023-06-20 10:12:51 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { SliderSecTwo } from "./components/SliderSecTwo";
+import Project3d from "./components/Project3d";
 function App() {
   return (
     <div className="App">
       <SliderSecTwo />
+      <Project3d />
     </div>
   );
 }
